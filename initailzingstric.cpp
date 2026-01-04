4b9d31c1176bb97ccda73918c5b125b2ad62a7e9
Maaz hassan <maazhassanmaazhassan1@gmail.com>
2026-01-04 20:11:37 +0500

    change name

--- initailzingstric.cpp
+++ initailzingstric.cpp
@@ -7,7 +7,7 @@ struct studentinfo{
 };
 
 int main() {
-    studentinfo s1{"Maaz hassan",18,3610303972197};//you can write any word instead of s1
+    studentinfo s1{"M",18,3610303972197};//you can write any word instead of s1
     cout<<"Name: "<<s1.name<<endl;
     cout<<"Age: "<<s1.age<<endl;
     cout<<"CNIC: "<<s1.cnic<<endl;
